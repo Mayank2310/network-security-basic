b5472473a41f46052bd2c2259bd3adbeb89bcee0
Mayank saini <mayankrajoriya2310@gmail.com>
2026-05-10 21:20:04 +0530

    More of data validation

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -1,6 +1,6 @@
 import yaml
 from networksecurity.exception.exception import NetworkSecurityException
-from networksecurity.logger.logger import logging
+from networksecurity.logging.logger import logging
 
 import os,sys
 import numpy as np
@@ -10,6 +10,17 @@ import pickle
 def read_yaml_file(file_path: str) -> dict:
     try:
         with open(file_path, "rb") as yaml_file:
-            return ymal.safe_load(yaml_file)
+            return yaml.safe_load(yaml_file)
     except Exception as e:
         raise NetworkSecurityException(e,sys) from e
+
+def write_yaml_file(file_path: str, content : object, replace: bool = False)-> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
